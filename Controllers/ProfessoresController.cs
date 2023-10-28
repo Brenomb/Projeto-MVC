924b16cfcd72b4b7865fe16fb6484a74d8963ac6
Brenomb <56926880+Brenomb@users.noreply.github.com>
2023-10-27 21:37:19 -0300

    Add files via upload

---
+++ Controllers/ProfessoresController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcDemo.Models;
+
+namespace mvcDemo.Controllers
+{
+    public class ProfessoresController : Controller
+    {
+        private readonly List<Professores> professores = new List<Professores>();
+
+        public ProfessoresController()
+        {
+            professores.Add(new Professores
+            {
+                Ra = "696969",
+                Nome = "Claudio Nunes",
+                Materia = "Engenharia de Software",
+                Turno = "Noturno",
+                Salario = 7000
+            });
+
+            professores.Add(new Professores
+            {
+                Ra = "011010",
+                Nome = "Fernando Branquinho",
+                Materia = "Prática de programação",
+                Turno = "Noturno",
+                Salario = 10000
+            });
+        }
+
+        public IActionResult Index()
+        {
+            return View(professores);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var professor = professores.Find(a => a.Ra == id);
+            if (professor == null) return NotFound();
+
+            return View(professor);
+        }
+    }
+}
+
---
+++ Models/Professores.cs
@@ -0,0 +1,11 @@
+﻿namespace mvcDemo.Models
+{
+    public class Professores
+    {
+        public string Ra { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+        public string Materia { get; set; } = string.Empty;
+        public string Turno { get; set; } = string.Empty;
+        public double Salario { get; set; }
+    }
+}
